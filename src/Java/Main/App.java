2ebef1060fa7b99a2c95d04b415bbf86674eab09
MattyMar234 <mattymar2002@gmail.com>
2022-11-07 12:12:22 +0100

    changes

---
+++ src/Java/Main/App.java
@@ -0,0 +1,23 @@
+package Java.Main;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+
+
+public class App extends Application{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+}
