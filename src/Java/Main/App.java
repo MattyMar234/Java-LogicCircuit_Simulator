98df8a086152d4fac7fed8a65aecab37b0daaa42
MattyMar234 <mattymar2002@gmail.com>
2022-11-07 23:00:59 +0100

    modifiche

--- src/Java/Main/App.java
+++ src/Java/Main/App.java
@@ -1,11 +1,18 @@
 package Java.Main;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 
@@ -13,15 +20,66 @@ import javafx.stage.Stage;
 public class App extends Application
 {
     public static final String Directory    = System.getProperty("user.dir");
-    public static final String FileFXML = Directory + "\\src\\fxml\\ApplicationPageController.fxml"
+    public static final String DirectoryFiles_FXML = Directory + "\\src\\fxml";
+
+    public static HashMap<String, String> FxmlFile_Paths = new HashMap<String, String>();
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Running...");
         launch(args);
     }
 
     @Override
-    public void start(Stage state) throws Exception {
-        // TODO Auto-generated method stub
+    public void start(Stage stage) throws Exception 
+    {
+        try { 
+
+            //************ Caricamento File FXML ************//
+            File[] files = new File(DirectoryFiles_FXML).listFiles();
+
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".fxml")) {
+                    FxmlFile_Paths.put(file.getName().replace(".fxml", ""), "fxml/" + file.getName());
         
+                }
+            }
+
+            //************ Impostazioni pagina ************//
+            
+            stage.setTitle("Circuit Simulator");
+            SetStage(stage, "ApplicationPage");
+            stage.show();
+
+            //lambda function 
+            stage.setOnCloseRequest(event -> {
+                event.consume();
+                System.exit(0);
+                
+            });
+
+            
+
+            System.out.println("starting stage"); 
+        } catch(NullPointerException e) {
+            System.out.println("File non trovato, errore nel percorso del file fxml ( URL = null ?)\nError: ");
+            System.out.println(e);
+
+        } catch(Exception e)  {
+           e.printStackTrace();
+        }
+    }
+
+    public void SetStage(Stage stage, String name) throws IOException, NullPointerException 
+    {
+        System.out.println("Loading: " + FxmlFile_Paths.get(name));
+        URL fxmlLocation = getClass().getClassLoader().getResource(FxmlFile_Paths.get(name));
+        FXMLLoader loader = new FXMLLoader(fxmlLocation);
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+    }
+
+    public FXMLLoader getStageFXMLLoader(String name) throws IOException  {
+        return new FXMLLoader(getClass().getClassLoader().getResource(FxmlFile_Paths.get(name)));
     }
 }
--- src/Java/Main/ApplicationPageController.java
+++ src/Java/Main/ApplicationPageController.java
@@ -1,5 +1,37 @@
 package Java.Main;
 
-public class ApplicationPageController {
-    
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+
+public class ApplicationPageController implements Initializable {
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private AnchorPane windowBack;
+
+    @FXML
+    private AnchorPane sideBar;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.strokeLine(40, 10, 10, 40);
+
+        
+    }
+
 }
